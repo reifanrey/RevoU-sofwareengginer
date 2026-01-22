019181b7ab89fce1cc46fecf34c6727cceca9930
reifanrey <reifanrey@gmail.com>
2026-01-22 21:36:22 +0700

    Pogress kofigurasi

---
+++ Mini-project-RevoU/js/to-do.js
@@ -0,0 +1,14 @@
+
+//Temporary storage for todo items
+lettodos = [];
+
+// Function to add a new  todo items
+function addTodo() {
+
+}
+function deleteAllTodo() {
+
+}
+function filterTodo() {
+
+}
