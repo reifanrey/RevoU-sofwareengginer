4fba6eeade6bf3968c6653752b4bd2f30fcfcdb1
reifanrey <reifanrey@gmail.com>
2026-01-22 23:39:35 +0700

    Pogress sampe ke script js

--- Mini-project-RevoU/js/to-do.js
+++ Mini-project-RevoU/js/to-do.js
@@ -1,14 +1,37 @@
 
-//Temporary storage for todo items
-lettodos = [];
+let todos = [];
 
-// Function to add a new  todo items
+const form = document.getElementById('todo-Form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    addTodo();
+});
+
 function addTodo() {
+    const todoText = document.getElementById('todoText').value;
+    const todoDate = document.getElementById('todoDate').value;
 
+    const newTodo = {
+        todo: todoText,
+        date: todoDate,
+        status: 'pending'
+    };
+
+    todos.push(newTodo);
+    console.log('Todo added:', todos);
+
+    document.getElementById('todoText').value = '';
+    document.getElementById('todoDate').value = '';
 }
+
+
+
+
 function deleteAllTodo() {
 
 }
 function filterTodo() {
 
 }
+
